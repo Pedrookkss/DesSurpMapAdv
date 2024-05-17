b5fbb72b06eebbd4192c2b1914008ac5f9262d6e
Pedrookkss <pha.bittencourt@gmail.com>
2024-05-17 13:40:03 -0300

    desafioSurpresa

---
+++ index.js
@@ -0,0 +1,18 @@
+let timePokemon =[
+    ["Pikachu", " Lv2"],
+    ["Charmander", " Lv2"]
+]
+
+let pikachu = timePokemon[0][0] + timePokemon[0][1]
+let charmander = timePokemon[1][0] + timePokemon[1][1]
+
+let slot = 2
+
+switch(slot){
+case 1:
+console.log(pikachu +" cadastrado com sucesso!")
+break
+case 2:
+console.log(charmander +" cadastrado com sucesso!")
+break
+}
